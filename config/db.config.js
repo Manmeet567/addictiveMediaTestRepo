bd55a81c31684eab3ee0b3b6ffe1f84c9786beb4
Manmeet567 <74705756+Manmeet567@users.noreply.github.com>
2022-11-09 17:26:56 +0530

    Add files via upload

---
+++ config/db.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+
+    HOST: "sql12.freemysqlhosting.net",
+    
+    USER: "sql12549755",
+    
+    PASSWORD: "3vbjFtAccu",
+    
+    DB: "sql12549755",
+    
+    dialect: "mysql",
+    
+    pool: {
+    
+    max: 5,
+    
+    min: 0,
+    
+    acquire: 30000,
+    
+    idle: 10000
+    
+    }
+    
+    };
